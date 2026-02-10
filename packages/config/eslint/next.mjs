8942552039c8d06653d4d0618d06f8571f21e482
Pablo Silva <pablosilva@192.168.1.12>
2026-02-10 12:13:26 -0300

    Fix lint format

--- packages/config/eslint/next.mjs
+++ packages/config/eslint/next.mjs
@@ -1,14 +1,14 @@
 // config-base.mjs
-import js from "@eslint/js";
-import ts from "typescript-eslint";
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
 
 export default [
   js.configs.recommended,
   ...ts.configs.recommended,
   {
     rules: {
-      "no-console": "warn",
-      "semi": ["error", "always"],
+      'no-console': 'warn',
+      semi: ['error', 'always'],
     },
   },
 ];
